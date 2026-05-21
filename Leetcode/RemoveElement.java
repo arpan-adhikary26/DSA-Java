d7d47f5e460cdd6158a2b9c16d5bc7938d1064d9
ARPAN ADHIKARY <arpanadhikary26@gmail.com>
2026-05-21 23:17:44 +0530

    Solved remove element leetcode 27

---
+++ Leetcode/RemoveElement.java
@@ -0,0 +1,18 @@
+/*
+Problem: 27-Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+*/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
