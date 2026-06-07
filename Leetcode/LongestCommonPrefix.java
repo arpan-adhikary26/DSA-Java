59606d7241e81326edec21b430545f57448ddd88
arpan-adhikary26 <arpanadhikary26@gmail.com>
2026-06-08 01:43:39 +0530

    Solve Longest Common Prefix using string comparison

---
+++ Leetcode/LongestCommonPrefix.java
@@ -0,0 +1,36 @@
+    // LeetCode 14 - Longest Common Prefix
+    //
+    // Find the longest common prefix string
+    // among an array of strings.
+    //
+    // Time Complexity:
+    //
+    // Space Complexity:
+
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        String prefix = strs[0];
+
+        for(int i = 1; i < strs.length; i++) {
+
+            while(strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if(prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(strs));
+
+    }
+}
