af805f35165d3c0ff3dd1ff4cd0996c707efbca9
arpan-adhikary26 <arpanadhikary26@gmail.com>
2026-06-28 23:35:42 +0530

    Updated: Solve Largest Odd Number in String using reverse traversal

--- Leetcode/LargestOddNumberInString.java
+++ Leetcode/LargestOddNumberInString.java
@@ -11,14 +11,13 @@
 
 class Solution {
     public String largestOddNumber(String num) {
-
-        for(int i=num.length()-1; i>=0; i--){
-           
-            int digit = num.charAt(i)-'0';
+        int n = num.length()-1;
+        while(n>=0){
+            int digit = num.charAt(n)-'0';
             if(digit%2!=0){
-                return num.substring(0, i + 1);
+                return num.substring(0,n+1);
             }
-
+            n--;
         }
         return "";
     }
